dc35f9c015c4a1385817a6ef1607ef19e7b6cde0
Stephen <93400341+StephenMCode@users.noreply.github.com>
2025-02-12 21:31:21 -0800

    Add project files.

---
+++ ConFPS.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+
+int main() {
+	cout << "TEMPLATE CODE!!!" << endl;
+
+	return 0;
+}
